a6cd4085f5385bdf91410e25651e68d0232b02a0
aagams2Shah <111478880+aagams2Shah@users.noreply.github.com>
2023-01-28 11:48:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function log(message){
-    document.getElementById("log").textContent += message + "\n"
 
-}
-
-log("Hello World")
-
-var b = Bugout("A")
-b.on("seen",function(address){
-    log(address + " [seen]");
-} );
-
-log(b.address() + " [me]");
